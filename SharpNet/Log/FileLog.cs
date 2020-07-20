4445cea9a9c5936585a46d020ab1a9e2bbfe62f2
gitstar333 <gitstar333@hotmail.com>
2020-07-20 17:50:02 +0900

    ExecuteAsAdmin  함수 추가

---
+++ SharpNet/Log/FileLog.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SharpNet.Log
+{
+    public class FileLog
+    {
+        private static FileLog instance;
+        private const int MaxCharWithinOneLine = 2000;
+
+        public RichTextBox richTextBox { get; set; }
+
+        public static FileLog Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new FileLog();
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// 콘셜 로그를 남긴다.
+        /// </summary>
+        /// <param name="logMsg"></param>
+        public void ConsolLog(string logMsg)
+        {
+#if DEBUG
+            Console.WriteLine(logMsg);
+#else
+    //Console.WriteLine("Mode=Release"); 
+#endif
+        }
+
+        public void Debug(string methodName, string logData, string className = null, bool isEnable = true)
+        {
+            SharpLog.Debug(methodName, logData, className, isEnable);
+        }
+
+        public void Error(string methodName, string logData, string className = null, bool isEnable = true)
+        {
+            SharpLog.Error(methodName, logData, className, isEnable);
+        }
+
+        public void DisplayLog(string strResult, Color color = default(Color), bool isEnable = true)
+        {
+            if (!isEnable)
+                return;
+
+            string str = string.Format("{0}:<{1}>\r\n", DateTime.Now.ToString("MM/dd hh:mm:ss"), strResult);
+
+            try
+            {
+                richTextBox.Invoke(new MethodInvoker(delegate ()
+                {
+                    if (richTextBox.Text.Length >= MaxCharWithinOneLine)
+                        richTextBox.Text.Remove(0, richTextBox.Text.IndexOf('\r') + 2);
+
+                    richTextBox.AppendText(str);
+                    richTextBox.ScrollToCaret();
+
+                    Debug("", strResult);
+
+                }));
+            }
+            catch (Exception ex)
+            {
+                SharpLog.Fatal("DisplayLog", ex.ToString());
+            }
+        }
+
+        public void DisplayLogClean()
+        {
+            richTextBox.Invoke(new MethodInvoker(delegate ()
+            {
+                richTextBox.Text = "";
+            }));
+        }
+
+
+        //public static void AppendText(this RichTextBox box, string str, Color color)
+        //{
+        //    str = string.Format("{0}:<{1}>\r\n", DateTime.Now.ToString("MM/dd hh:mm:ss"), str);
+
+        //    if (box.Text.Length >= MaxCharWithinOneLine)
+        //        box.Text.Remove(0, box.Text.IndexOf('\r') + 2);
+
+        //    box.SelectionStart = box.TextLength;
+        //    box.SelectionLength = 0;
+
+        //    box.SelectionColor = color;
+        //    box.AppendText(str);
+        //    box.SelectionColor = box.ForeColor;
+        //}
+
+
+        public static void InvokeIfRequired(Control control, MethodInvoker action)
+        {
+            try
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(action);
+                }
+                else
+                {
+                    action();
+                }
+            }
+            catch { }
+        }
+
+    }
+}
---
+++ SharpNet/Log/SharpLog.cs
@@ -0,0 +1,192 @@
+﻿using NLog;
+using System;
+using System.Runtime.Serialization;
+using System.Windows.Forms;
+
+namespace SharpNet.Log
+{
+    public static class SharpLog
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        // private static Logger logger = LogManager.GetLogger("Test");
+        //private static Logger logger = LogManager.CreateNullLogger();
+        //logger.Log(LogLevel.Info, "Sample fatal error message, k={0}, l={1}", k, l);
+
+
+        ///// <summary>
+        ///// write Tracer log
+        ///// </summary>
+        ///// <param name="methodName"></param>
+        ///// <param name="logData"></param>
+        ///// <param name="className"></param>
+        public static void Tracer(string methodName, string logData, string className = null, bool isEnable = true)
+        {
+            if (!isEnable)
+                return;
+
+            if (!string.IsNullOrEmpty(className))
+                logger = LogManager.GetLogger(className);
+
+            logger.Trace("{0}|{1}", methodName, logData);
+        }
+
+        /// <summary>
+        /// write Debug log
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="logData"></param>
+        /// <param name="className"></param>
+        public static void Debug(string methodName, string logData, string className = null, bool isEnable = false)
+        {
+            if (!isEnable)
+                return;
+
+            if (!string.IsNullOrEmpty(className))
+                logger = LogManager.GetLogger(className);
+
+            logger.Debug("{0}|{1}", methodName, logData);
+        }
+
+        /// <summary>
+        /// Write Info log
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="logData"></param>
+        /// <param name="className"></param>
+        public static void Info(string methodName, string logData, string className = null, bool isEnable = true)
+        {
+            if (!isEnable)
+                return;
+
+            if (!string.IsNullOrEmpty(className))
+                logger = LogManager.GetLogger(className);
+
+            logger.Info("{0}|{1}", methodName, logData);
+        }
+
+        /// <summary>
+        /// Write Waring Log
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="logData"></param>
+        /// <param name="className"></param>
+        public static void Waring(string methodName, string logData, string className = null, bool isEnable = true)
+        {
+            if (!isEnable)
+                return;
+
+            if (!string.IsNullOrEmpty(className))
+                logger = LogManager.GetLogger(className);
+
+            logger.Warn("{0}|{1}", methodName, logData);
+        }
+
+        /// <summary>
+        /// Write Error Log
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="logData"></param>
+        /// <param name="className"></param>
+        public static void Error(string methodName, string logData, string className = null, bool isEnable = true)
+        {
+            if (!isEnable)
+                return;
+
+            if (!string.IsNullOrEmpty(className))
+                logger = LogManager.GetLogger(className);
+
+            logger.Error("{0}|{1}", methodName, logData);
+        }
+
+        /// <summary>
+        /// Write Fatal Log
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="logData"></param>
+        /// <param name="className"></param>
+        public static void Fatal(string methodName, string logData, string className = null , bool isEnable = true)
+        {
+            if (!isEnable)
+                return;
+
+            if (!string.IsNullOrEmpty(className))
+                logger = LogManager.GetLogger(className);
+
+            logger.Fatal("{0}|{1}", methodName, logData);
+        }
+
+        /// <summary>
+        /// Write Form View Message Log (trace 사용/ UI 출력을 위하여 사용  )
+        /// </summary>
+        /// <param name="methodName">로그인/게임탈퇴 등.</param>
+        /// <param name="logData">아디/암호/금액 등.</param>
+        /// <param name="className">User/DB 등으로 표기</param>
+        public static void View(string methodName, string logData, string className = null, bool isEnable = true)
+        {
+            if (!isEnable)
+                return;
+
+            if (!string.IsNullOrEmpty(className))
+                logger = LogManager.GetLogger(className);
+
+            logger.Debug("{0}|{1}", methodName, logData);
+        }
+
+        /// <summary>
+        /// //Send email log (info 사용) / 날자.시간을 포함할것
+        /// </summary>
+        /// <param name="methodName">충전/환전 등..</param>
+        /// <param name="logData"> 날자,시간을 포함할것.</param>
+        /// <param name="className"></param>
+        public static void Mail(string methodName, string logData, string className = null, bool isEnable = true)
+        {
+            if (!isEnable)
+                return;
+
+            if (string.IsNullOrEmpty(className))
+                logger = LogManager.GetLogger("Mail");
+            else
+                logger = LogManager.GetLogger(className);
+
+            logger.Info("{0}|{1}", methodName, logData);
+        }
+
+        /// <summary>
+        /// ShowMessageBox in TopMost Method
+        /// </summary>
+        /// <param name="desc"></param>
+        /// <param name="caption"></param>
+        public static void ShowMessageBox(string desc, string caption = null, bool isEnable = true)
+        {
+            if (!isEnable)
+                return;
+
+            if (string.IsNullOrEmpty(caption))
+                MessageBox.Show(new Form { TopMost = true }, desc);
+            else
+                MessageBox.Show(new Form { TopMost = true }, desc, caption);
+        }
+
+        /// <summary>
+        /// log를 남기고 메세지 보인다.
+        /// </summary>
+        /// <param name="desc"></param>
+        /// <param name="caption"></param>
+        public static void ShowMessageBoxWithLog(string desc, string caption = null, bool isEnable = true)
+        {
+            if (!isEnable)
+                return;
+
+            if (string.IsNullOrEmpty(caption))
+            {
+                Debug("SharpLog", desc);
+                MessageBox.Show(new Form { TopMost = true }, desc);
+            }
+            else
+            {
+                Debug(caption, desc);
+                MessageBox.Show(new Form { TopMost = true }, desc, caption);
+            }
+        }
+    }
+}
